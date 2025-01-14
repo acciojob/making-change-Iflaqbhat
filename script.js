c902e55664903827e9d672a6020334a199039063
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2025-01-14 11:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const makeChange = (c) => {
-    // Calculate the number of quarters
     const q = Math.floor(c / 25);
-    c %= 25; // Remaining cents after quarters
-
-    // Calculate the number of dimes
+    c %= 25; 
     const d = Math.floor(c / 10);
     c %= 10; // Remaining cents after dimes
 
     // Calculate the number of nickels
     const n = Math.floor(c / 5);
-    c %= 5; // Remaining cents after nickels
-
-    // Remaining cents are all pennies
+    c %= 5; 
     const p = c;
 
-    // Return the object with the counts
     return { q, d, n, p };
 };
+module.exports=mincost;
 
 	
 
